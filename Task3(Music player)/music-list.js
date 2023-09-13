43ad3e3196516e3509a824075fbece5f7690c6a3
sukhmanpreetsinghjaswal <144266426+sukhmanpreetsinghjaswal@users.noreply.github.com>
2023-09-13 13:20:54 +0530

    Add files via upload

---
+++ Task3(Music player)/music-list.js
@@ -0,0 +1,27 @@
+
+let allMusic = [
+  {
+    name: "Still Rollin",
+    artist: "Shubh",
+    images: "music-1.jpeg",
+    src: "music-1"
+  },
+  {
+    name: "Rubicon Drill",
+    artist: "Parmish Verma",
+    images: "music-2.jpeg",
+    src: "music-2"
+  },
+  {
+    name: "Boot Cut",
+    artist: "Prem Dhillon",
+    images: "music-3.jpeg",
+    src: "music-3"
+  },
+  {
+    name: "Schedule",
+    artist: "Tegi Pannu",
+    images: "music-4.jpeg",
+    src: "music-4"
+  },
+];
